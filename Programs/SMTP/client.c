a2718452010e112a65e8185ce453c95bb78b22b0
Aadithya Sai <aadithyas0610@gmail.com>
2024-06-22 21:15:08 +0530

    Basic SMTP Implemented

--- Programs/SMTP/client.c
+++ Programs/SMTP/client.c
@@ -4,14 +4,11 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-int main()
-{
-
+int main() {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1)
-    {
-        printf("socket creation failed...\n");
-        exit(-1);
+    if (sockfd == -1) {
+      printf("socket creation failed...\n");
+      exit(-1);
     }
     printf("Socket successfully created..\n");
 
@@ -21,11 +18,45 @@ int main()
     servaddr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
     unsigned int len = sizeof(servaddr);
-    if (connect(sockfd, (struct sockaddr *)&servaddr, len))
-    {
+    if (connect(sockfd, (struct sockaddr *)&servaddr, len)) {
         printf("Socket connection failed... Is the server running?\n");
         exit(-1);
     }
     printf("Client Connected...\n");
-    // TODO: Implement SMTP client logic here
+
+    char cliaddr[64], sendaddr[64], recvaddr[64], data[256];
+    char cmd[256];
+
+    // SMTP Logic    
+    printf("Enter client email address: ");
+    scanf("%s", cliaddr);
+    sprintf(cmd, "HELO %s", cliaddr);
+    write(sockfd, cmd, sizeof(cmd));
+
+    printf("Enter sender email address: ");
+    scanf("%s", sendaddr);
+    sprintf(cmd, "MAILFROM %s", sendaddr);
+    write(sockfd, cmd, sizeof(cmd));
+
+    printf("Enter recipient email address: ");
+    scanf("%s", recvaddr);
+    sprintf(cmd, "RCPTTO %s", recvaddr);
+    write(sockfd, cmd, sizeof(cmd));
+
+    write(sockfd, "DATA", sizeof("DATA"));
+    printf("Enter subject: ");
+    scanf("\n%[^\n]", data);
+    sprintf(cmd, "SUB: %s", data);
+    write(sockfd, cmd, sizeof(cmd));
+
+    printf("Enter message: ");
+    scanf("\n%[^\n]", data);
+    write(sockfd, data, sizeof(data));
+
+    write(sockfd, ".", sizeof("."));
+
+    write(sockfd, "QUIT", sizeof("QUIT"));
+
+
+    close(sockfd);
 }
--- Programs/SMTP/server.c
+++ Programs/SMTP/server.c
@@ -5,12 +5,9 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-int main()
-{
-
+int main() {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1)
-    {
+    if (sockfd == -1) {
         printf("Socket creation failed...\n");
         exit(-1);
     }
@@ -21,15 +18,13 @@ int main()
     servaddr.sin_port = htons(8000);
     servaddr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
-    if (bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) != 0)
-    {
+    if (bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) != 0) {
         printf("Socket bind failed...\n");
         exit(-1);
     }
     printf("Socket successfully binded..\n");
 
-    if ((listen(sockfd, 255)) != 0)
-    {
+    if ((listen(sockfd, 255)) != 0) {
         printf("Listening failed...\n");
         exit(-1);
     }
@@ -39,11 +34,58 @@ int main()
     unsigned int len = sizeof(cli);
     int connfd = accept(sockfd, (struct sockaddr *)&cli, &len);
 
-    if (connfd < 0)
-    {
+    if (connfd < 0) {
         printf("Server accept failed...\n");
         exit(-1);
     }
-    printf("Server accepted the client...\n");
-    // TODO: Implement SMTP server logic here
+
+    // SMTP Logic
+
+    char msg[256], *cmd, *data;
+
+    // HELO <client addr>
+    do {
+        read(connfd, &msg, sizeof(msg));
+        data = &msg[0];
+        cmd = strsep(&data, " ");
+    } while (strcmp(cmd, "HELO") != 0);
+    printf("HELO command received from %s\n", data);
+
+    // MAILFROM: <sender email addr>
+    do {
+        read(connfd, &msg, sizeof(msg));
+        data = &msg[0];
+        cmd = strsep(&data, " ");
+    } while (strcmp(cmd, "MAILFROM") != 0);
+    printf("MAIL FROM %s\n", data);
+
+    // RCPTTO: <recipient email addr>
+    do {
+        read(connfd, &msg, sizeof(msg));
+        data = &msg[0];
+        cmd = strsep(&data, " ");
+    } while (strcmp(cmd, "RCPTTO") != 0);
+    printf("RCPT TO %s\n", data);
+
+    // DATA
+    do {
+        read(connfd, &msg, sizeof(msg));
+    } while (strcmp(msg, "DATA") != 0);
+    printf("DATA command received\n");
+
+    do {
+        read(connfd, &msg, sizeof(msg));
+        printf("%s\n", msg);
+        fflush(stdout);
+    } while (strcmp(msg, ".") != 0);
+    printf("End of message\n");
+
+    // QUIT
+    do {
+        read(connfd, &msg, sizeof(msg));
+    } while (strcmp(msg, "QUIT") != 0);
+    printf("Client Closing\n");
+
+    close(sockfd);
+    close(connfd);
 }
